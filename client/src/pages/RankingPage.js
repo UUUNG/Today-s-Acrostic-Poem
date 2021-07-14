01f204d32f2188c79fc0f1522a87f9d19203bcf6
UUUNG <jeawoong123@naver.com>
2021-07-14 21:45:21 +0900

    뭐지 이상하게 안올라간 커밋들..?

--- client/src/pages/RankingPage.js
+++ client/src/pages/RankingPage.js
@@ -1,27 +1,13 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import Container from '@material-ui/core/Container';
 import RankingContainer from '../components/organisms/RankingContainer'
 const RankingPage = () => {
-  const [rankData, setRankData]=React.useState(null);
-  
-  const callApi = async()=>{
-    const response = await fetch('/RankingPage');
-    const body = await response.json();
-    return body;
-  }
-
-  useEffect(()=>{
-      callApi()
-      .then(res=>setRankData(res[0]))
-      .catch(err=>console.log(err));
-  }, []);
 
   return (
     <div>
-      {rankData ? <Container maxWidth="sm">
-        <RankingContainer props = {rankData}/>
-      </Container> : <div></div>}
-      
+      <Container maxWidth="sm">
+        <RankingContainer/>
+      </Container>
     </div>
 
   )
